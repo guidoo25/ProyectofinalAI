eb27b3b8402f7e61db44762c9c70314f389611fb
guido gutierrez <121067148+guidoo25@users.noreply.github.com>
2023-08-30 15:59:54 -0500

    Update mail.js

--- server-node/services/mail.js
+++ server-node/services/mail.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: 'guidoroberto.25@gmail.com',
-      pass: 'awzzeaeokqlnxlbn'
+      user: 'exmaple.25@gmail.com',
+      pass: '*******'
     }
   });
 
